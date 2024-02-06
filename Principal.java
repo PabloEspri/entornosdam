97001ddb3b8db32278d1fb7a3fd8a389938325be
dam23-04 <dam23-04@c16x09.iescierva.net>
2024-02-06 09:54:56 +0100

    ED1 Nueva clase Usuario + Nueva funcionalidad para la clase Principal

--- Principal.java
+++ Principal.java
@@ -1,10 +1,46 @@
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Principal {
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         mostrarFechaHoraActual();
+
+        int cantidad = 0;
+        boolean ok = false;
+
+        while (ok!= true) {
+            try {
+                System.out.print("Introduzca la cantidad de usuarios que quiere crear: ");
+                cantidad = sc.nextInt();
+                sc.nextLine();
+                if(cantidad > 0){
+                    ok = true;
+                }else{
+                    System.out.println("Debe de introducir un número mayor a 0");
+                }
+            } catch (Exception e) {
+                sc.nextLine();
+                System.out.println("Debe de introducir un número entero");
+            }
+        }
+        
+
+        Usuario[] usuarios = creaArrayUsuarios(cantidad);
+
+        System.out.println();
+        System.out.println("---------------------------");
+        System.out.println("Usuarios Creados");
+        System.out.println("---------------------------");
+        for(Usuario usuario : usuarios){
+            System.out.println("Nombre del Usuario: "+ usuario.nombre);
+            System.out.println("Apellidos del Usuario: "+ usuario.apellidos);
+            System.out.println("Email del Usuario: "+ usuario.email);
+            System.out.println();
+        }
+
     }
 
     public static void mostrarFechaHoraActual() {
@@ -12,6 +48,32 @@ public class Principal {
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         String fechaHoraFormateada = formato.format(fechaHoraActual);
         System.out.println("Fecha y hora actual: " + fechaHoraFormateada);
+    }
+
+    public static Usuario[] creaArrayUsuarios(int cantidad){
+        Usuario[] usuarios = new Usuario[cantidad];
+
+        String nombre;
+        String apellidos;
+        String email;
+
+        System.out.println();
+        System.out.println("---------------------------");
+        System.out.println("Creación de Usuarios");
+        System.out.println("---------------------------");
+        for(int i = 0; i < cantidad; i++){
+            System.out.print("Nombre: ");
+            nombre = sc.nextLine();
+            System.out.print("Apellidos: ");
+            apellidos = sc.nextLine();
+            System.out.print("Email: ");
+            email = sc.nextLine();
+
+            System.out.println();
+            usuarios[i] = new Usuario(nombre, apellidos, email);
+        }
+
+        return usuarios;
     }
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ***entornosdam***
 ## _Nuevo metodo que enseña el tiempo y la hora actual en el archivo Principal.java_
+## _Creación de una nueva clase Usuario, la cual tiene tres atributos_
+## _Nueva funcion para la clase Principal, creación de usuarios_
 Repositorio de prueba 1ºDAM
