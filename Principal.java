ce1fffef96f1b85f213b778cd9bb41fc7b15448d
dam23-04 <dam23-04@c16x09.iescierva.net>
2024-02-06 09:04:44 +0100

    ED1 Nueva rama de trabajo + Nueva funcionalidad para el archivo Principal.java

--- Principal.java
+++ Principal.java
@@ -1,5 +1,17 @@
-public class Principal{
-	public static void main(String[]args){
-		System.out.println("Hola mundo");	
-	}
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        mostrarFechaHoraActual();
+    }
+
+    public static void mostrarFechaHoraActual() {
+        Date fechaHoraActual = new Date();
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String fechaHoraFormateada = formato.format(fechaHoraActual);
+        System.out.println("Fecha y hora actual: " + fechaHoraFormateada);
+    }
 }
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# entornosdam
+# ***entornosdam***
+## _Nuevo metodo que enseña el tiempo y la hora actual en el archivo Principal.java_
 Repositorio de prueba 1ºDAM
